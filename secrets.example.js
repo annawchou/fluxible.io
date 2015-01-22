29d933960fb47515bb8d39aece19c209fe84b776
jedireza <reza@akhavan.me>
2015-01-22 09:55:36 -0800

    updated setup instructions

--- secrets.example.js
+++ secrets.example.js
@@ -1,6 +1,6 @@
 module.exports = {
     github: {
-        clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET
+        clientId: process.env.GITHUB_CLIENT_ID || '',
+        clientSecret: process.env.GITHUB_CLIENT_SECRET || ''
     }
 };
