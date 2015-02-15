1ddfce383f8c1e1a7379012dc1a594c2519db8c0
sbertal <sbertal@yahoo-inc.com>
2015-02-15 10:58:01 -0800

    add devtest to precommit

--- utils/createAPIWhitelist.js
+++ utils/createAPIWhitelist.js
@@ -3,10 +3,6 @@
  * Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 module.exports = function createAPIWhitelistFromConfig(config) {
-    if (!config) {
-        return [];
-    }
-
     return config.reduce(function (whitelist, item) {
         item.children.forEach(function (api) {
             whitelist[api.navParams.slug] = api;
