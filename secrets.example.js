d7b7c4d5ebd57dc2acf453e0670d7fe24631f0bd
sbertal <sbertal@yahoo-inc.com>
2015-03-21 09:27:37 -0700

    [skip ci] add secret docs

--- secrets.example.js
+++ secrets.example.js
@@ -1,3 +1,8 @@
+/**
+ * This is a sample file for your Github client id and client secret
+ * This is needed to collect the doc's from the fluxible repo.
+ * You can generate the client/secret pair here: https://github.com/settings/applications/new
+ */
 module.exports = {
     github: {
         clientId: process.env.GITHUB_CLIENT_ID || '',
